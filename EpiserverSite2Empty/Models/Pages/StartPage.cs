46190334080559139fb10b4382954f680ffdb8e6
kl4u5 <kl4u5.j3n53n@gmail.com>
2017-05-09 12:30:00 +0200

    Release1 bugfix

--- EpiserverSite2Empty/Models/Pages/StartPage.cs
+++ EpiserverSite2Empty/Models/Pages/StartPage.cs
@@ -1,4 +1,4 @@
-using System;
+﻿ng System;
 using System.ComponentModel.DataAnnotations;
 using EPiServer.Core;
 using EPiServer.DataAbstraction;
@@ -14,7 +14,7 @@ namespace EpiserverSite2Empty.Models.Pages
                 [CultureSpecific]
                 [Display(
                     Name = "Main body",
-                    Description = "The RR2 main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+                    Description = "The RR2  release1 main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
                     GroupName = SystemTabNames.Content,
                     Order = 1)]
                 public virtual XhtmlString MainBody { get; set; }
